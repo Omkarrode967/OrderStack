3f268acbdfb300c058390e5c68ed9aa088655ddc
Omkar Rode <omkarrode967@gmail.com>
2026-01-02 12:55:08 +0530

    Initial commit

---
+++ src/main/java/com/Omkar/SpringEcom/SpringEcomApplication.java
@@ -0,0 +1,16 @@
+package com.Omkar.SpringEcom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+public class SpringEcomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEcomApplication.class, args);
+	}
+	
+}
